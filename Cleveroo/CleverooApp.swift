4c7eaf48aaef4e52800565d3059ff6acdd9da7b4
Maya-Marzouki <Maya.Marzouki@esprit.tn>
2025-11-05 18:03:04 +0100

    Initial Commit

---
+++ Cleveroo/CleverooApp.swift
@@ -0,0 +1,21 @@
+//
+//  CleverooApp.swift
+//  Cleveroo
+//
+//  Created by Maya Marzouki on 5/11/2025.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct CleverooApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
